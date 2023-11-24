c33ff10d4c611213e1acbdd79a1e5067e09611c5
ginoya <rishitginoya@gmail.com>
2023-11-24 10:44:35 +0530

    Initial commit

---
+++ Assignment10/Controllers/LogController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment10.Controllers
+{
+    [Route("api/log")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<LogController> _logger;
+
+        public LogController(IConfiguration configuration, ILogger<LogController> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        [HttpGet("{message}/{logLevel}")]
+        public IActionResult Get(string message, string logLevel)
+        {
+            string levelFromConfig = _configuration.GetValue<string>("Logging:LogLevel:AllowedLevel");
+
+            if (string.IsNullOrEmpty(levelFromConfig))
+            {
+                return BadRequest($"Invalid log level provided. Allowed log level is {levelFromConfig}");
+            }
+            else
+            {
+                switch (logLevel.ToLower())
+                {
+                    case "debug":
+                        _logger.LogDebug($"Debug :: {message}");
+                        break;
+                    case "warning":
+                        _logger.LogWarning($"Warning :: {message}");
+                        break;
+                    case "error":
+                        _logger.LogError($"Error :: {message}");
+                        break;
+                    case "trace":
+                        _logger.LogTrace($"Trace :: {message}");
+                        break;
+                    case "information":
+                        _logger.LogInformation($"Information :: {message}");
+                        break;
+                    case "critical": 
+                        _logger.LogCritical($"Critical :: {message}");
+                        break;
+                    default:
+                        return BadRequest($"Invalid log level: {logLevel}");
+                }
+            }
+            
+            return Ok(new { logLevel = logLevel , isPrinted = true});
+        }
+    }
+}
---
+++ Assignment10/Startup.cs
@@ -0,0 +1,52 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
+
+namespace Assignment10
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Assignment10", Version = "v1" });
+            });
+            services.AddLogging(builder =>
+                builder.AddConfiguration(Configuration.GetSection("Logging"))
+                    .AddConsole()
+                    .AddDebug());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Assignment10 v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
